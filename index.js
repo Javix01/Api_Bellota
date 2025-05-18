3fb85e3898606ca156a33850b0197cfc41ecb5f7
Javix01 <90986905+Javix01@users.noreply.github.com>
2025-05-18 12:16:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const Incidencia = require('./models/Incidencia');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
+app.use(express.json({ limit: '20mb' }));  // Permite hasta 20MB de Base64
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
